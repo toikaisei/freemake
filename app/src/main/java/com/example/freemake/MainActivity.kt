f83f0b5cd1c3bb4f10dfb2e853deb02ad82754ea
toi <toi@freemake.co.jp>
2024-06-10 15:37:33 +0900

    数量表示、数量が変わる+-ボタン追加、現在時刻表示

--- app/src/main/java/com/example/freemake/MainActivity.kt
+++ app/src/main/java/com/example/freemake/MainActivity.kt
@@ -1,47 +1,92 @@
 package com.example.freemake
 
+import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.annotation.RequiresApi
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.freemake.ui.theme.FreeMakeTheme
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
+import java.text.SimpleDateFormat
+import java.time.LocalDate
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+import java.util.Date
 
 class MainActivity : ComponentActivity() {
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             FreeMakeTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    Column (modifier = Modifier.padding(innerPadding)) {
+                        Counter()
+                        TimeNow()
+                    }
+
                 }
+
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun Counter() {
+
+    Row() {
+        Text(
+            text = "数量",
+        )
+        var count by remember { mutableStateOf(0) }
+        Text(
+            text = "$count",
+        )
+        Button(
+            onClick = { count++ }
+        ) {
+            Text(text = "＋")
+        }
+        Button(
+            onClick = { count-- }
+        ) {
+            Text(text = "ー")
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     FreeMakeTheme {
-        Greeting("Android")
+        Counter()
     }
+}
+@RequiresApi(Build.VERSION_CODES.O)
+@Composable
+fun TimeNow() {
+    val dateAndtime: LocalDateTime = LocalDateTime.now()
+    val onlyDate: LocalDate = LocalDate.now()
+
+    Text("Current date and time: $dateAndtime")
+    Text("Current date: $onlyDate")
 }
